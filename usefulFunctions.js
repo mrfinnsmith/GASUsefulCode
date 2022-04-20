fe499365a1edfd29c3b39563761f3c7c6e141da4
Finn Smith <finnbarsmith@gmail.com>
2022-04-19 22:58:41 -0700

    Set up function to find a sheet by ID in a Google Sheet

---
+++ usefulFunctions.js
@@ -0,0 +1,15 @@
+function getSheetById(spreadsheet, targetSheetId) {
+  
+  let targetSheet;
+
+  let sheets = spreadsheet.getSheets();
+  for (i in sheets) {
+    let thisSheet = sheets[i];
+    let thisSheetId = thisSheet.getSheetId();
+    if (thisSheetId == targetSheetId) {
+      targetSheet = thisSheet;
+      break;
+    }
+  }
+  return targetSheet;
+}
