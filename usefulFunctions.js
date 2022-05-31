a5b2cd9f92cb156a778ca336d1f437324d674fdf
Finn Smith <finnbarsmith@gmail.com>
2022-05-31 14:46:40 -0700

    add jsdoc comments and documentation for getSheetById

--- usefulFunctions.js
+++ usefulFunctions.js
@@ -1,5 +1,15 @@
+/**
+ * This function accepts a spreadsheet to search and a target sheet ID. 
+ * It returns the sheet that the user is looking for if found in the 
+ * spreadsheet.
+ * 
+ * @param {spreadsheet} spreadsheet The spreadsheet object that the user is searching.
+ * @param {number} targetSheetId The ID number of the target sheet
+ * @returns {sheet} The sheet object that the user is looking for
+ */
+
 function getSheetById(spreadsheet, targetSheetId) {
-  
+  spreadsheet
   let targetSheet;
 
   let sheets = spreadsheet.getSheets();
